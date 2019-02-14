0ea08a41f01118675f410c9927dd4694be752398
mollerjorge <jmoller2106@gmail.com>
2019-02-14 20:09:46 -0300

    Finish refactoring

--- src/mock/works.js
+++ src/mock/works.js
@@ -43,7 +43,7 @@ const works = [
     desc: 'The #1 customer rated EPOS system on the market',
     workBackground: noblyWorkBg,
     client:
-      'Nobly is a point-of-sale, inventory management and CRM app for the iPad and android tablets. It offers an integral solution for coffee shops, restaurants and retail stores.',
+      'Nobly is a point-of-sale, inventory management and CRM app for the iPad and android tablets. It offers a complete solution for coffee shops, restaurants and retail stores.',
     project:
       'A cloud-based backoffice that manages the whole business. The application offers a variety of features, among the most important ones are: payment integration services with Paymentsense, Barclaycard, iZettle, stock and inventory tracking, loyalty and rewards system and the iPad grid configuration.',
     challenge:
@@ -57,9 +57,9 @@ const works = [
     desc: 'All in one solution for contact centers',
     workBackground: integraWorkBg,
     client:
-      'Integra provides an all in one solution to contact centers through uContact, their main web based platform.',
+      'Integra provides an all in one solution for contact centers through uContact, their main web based platform.',
     project:
-      'uConcact is a a web application that aims to reduce costs and improve the productivity of agents and supervisors within a contact center. Some of the features uContact has to offer are: Dialers (Inbound & Outbound) - SMS, Email & Webchat in a Unified Inbox - A Call Center Designed CRM - Completely Customizable Reporting - Gamification - Recording - Voice Broadcasting - Quality Control - Live Coaching (Spy, Whisper, Collaborate).',
+      'uContact is a a web application that aims to reduce costs and improve the productivity of agents and supervisors within a contact center. Some of the features uContact has to offer are: Dialers (Inbound & Outbound) - SMS, Email & Webchat in a Unified Inbox - A Call Center Designed CRM - Completely Customizable Reporting - Gamification - Recording - Voice Broadcasting - Quality Control - Live Coaching (Spy, Whisper, Collaborate).',
     challenge:
       'I was hired by integra to migrate the entire platform, from java fx, to HTML, CSS and javascript. One of the biggest challenges I had while working at Integra was to program the built in telephone of the application. For that we used javascript in the frontend connected to a java REST API. It was quite the challenge as I had to do a lot of research on topics related to telephony, such as asterisk servers, PBXs, Web RTC etc.',
     tools: ['Javascript', 'jQuery', 'HTML', 'CSS', 'Java', 'MySql'],
@@ -86,9 +86,9 @@ const works = [
     workBackground: novaWorkBg,
     client: null,
     project:
-      'Nova is an administrator template that I built with the purpose of selling it on www.themeforest.net',
+      'Nova is an administrator template built by me that can be used by anyone to start up a project.',
     challenge:
-      'The goal of this side project was to learn how to build things from scratch, that is why I did not want to use ui frameworks or pre built libraries. One of the challenges was to learn how to apply different javascript patterns to build the application in a modular fashion. Also I got to learn how to use SASS and BEM together which until this day I think is one of the best ways to write comprehensive and clean styles.',
+      'The goal of this side project was to learn how to build things from scratch, that is why I did not want to use ui frameworks or pre built libraries. One of the challenges was to learn how to apply different javascript patterns to build the application in a modular fashion. Also, I got to learn how to use SASS and BEM together which until this day I think is one of the best ways to write comprehensive and clean styles.',
     tools: ['Javascript', 'jQuery', 'HTML', 'SASS'],
     projectUrl: 'https://mollerjorge.github.io/nova/',
     images: [novaWork01, novaWork02, novaWork03, novaWork04, novaWork05]
@@ -102,21 +102,21 @@ const works = [
     project:
       'Sinergy is an e-commerce platform that allows the user to setup an entire store from scratch with ease. It allows anyone to sell online, issue invoices and import products from other e-commerce platforms such as aliexpress or DHGate. ',
     challenge:
-      "One of the challenges was to import products from aliexpress and DHGate. For that I built a web scrapper that retrieves the most relevant information of a product such as images, price, name and description and pulls up that information in the Create Product view, so that the user doesn't have to go through that mannually.",
+      "One of the challenges was to import products from aliexpress and DHGate. For that I built a web scrapper that retrieves the most relevant information of a product such as images, price, name and description and pulls up that information in the Create Product view, so that the user doesn't have to go through that manually.",
     tools: ['React', 'Redux', 'styled-components', 'AWS-S3', 'Node', 'Mongo'],
     images: [sinergyWork04, sinergyWork01, sinergyWork02, sinergyWork03]
   },
   {
     backgroundUrl: stCardBg,
     name: 'Technical Services',
-    desc: 'Service management solution for company: Technical Services',
+    desc: 'Service management solution',
     workBackground: stWorkBg,
     client:
-      "Technical services is a company based in Uruguay that provides it's customers with different technical services.",
+      'Technical services is a company based in Uruguay that provides its customers with different services.',
     project:
-      'The idea behind the project was to build a service management application that helped Technical Services to automate different processes within the company. Broadly speaking it allows the client to log in to the application and fill out a form to request a particular service. An administrator from Technical Services then logs in to the application and performs different actions to that request, such as assign it to an employee, attach images, schedule a day and time for the service to take place etc. The platform also allows to send a digital contract (pdf) to the client to sign it and approve de budget.',
+      'The idea behind the project was to build a service management application that helped Technical Services to automate different processes within the company. Broadly speaking, it allows the client to log in to the application and fill out a form to request a particular service. An administrator from Technical Services then logs in to the application and performs different actions to that request, such as assign it to an employee, attach images, schedule a day and time for the service to take place etc. The platform also allows to send a digital contract (pdf) to the client to review and approve the budget.',
     challenge:
-      'It was my first React project, so the main challenge was to put together everything I have learnt throughout the different courses I took of React. One of the challenges was to wire up the application with AWS-S3 to store images.',
+      'It was my first React project, so the main challenge was to put together everything I have learnt throughout the different React courses I took. Another challenge was to wire up the application with AWS-S3 to store images.',
     tools: ['React', 'Redux', 'styled-components', 'AWS-S3', 'Node', 'Mongo'],
     images: [stWork01, stWork02]
   }
