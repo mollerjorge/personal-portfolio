6b8b16d4c5ff30464a814d1670cc7e20393aaea8
mollerjorge <jmoller2106@gmail.com>
2019-02-16 14:46:31 -0300

    initial commit

---
+++ build/precache-manifest.860d4997da0e5948c6ccd8eb611668e7.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = [
+  {
+    "revision": "f621955eb7269be46ca2f564f12cc753",
+    "url": "/static/media/integraWork03.f621955e.jpg"
+  },
+  {
+    "revision": "e0cc8b21298efda07ba6",
+    "url": "/static/css/main.f0f0d356.chunk.css"
+  },
+  {
+    "revision": "0755e703241c0e7bda10",
+    "url": "/static/js/1.0755e703.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba631583f47522612fafdc39158d2d45",
+    "url": "/static/media/logo3.ba631583.svg"
+  },
+  {
+    "revision": "16117cde0ed661d22eb2d78a9722701b",
+    "url": "/static/media/astronaut.16117cde.svg"
+  },
+  {
+    "revision": "b0349b6d389c6b9a767205bb18427b93",
+    "url": "/static/media/astronaut1.b0349b6d.svg"
+  },
+  {
+    "revision": "1863e9066a855e0e4a8a26975d94df89",
+    "url": "/static/media/notebook.1863e906.svg"
+  },
+  {
+    "revision": "1a230b1bc04da72cf8229c61190c7fc3",
+    "url": "/static/media/linkedin.1a230b1b.svg"
+  },
+  {
+    "revision": "9b346c4ebea59cabf3702b81e08f3558",
+    "url": "/static/media/medium.9b346c4e.svg"
+  },
+  {
+    "revision": "da2aac1056e1e645efd8b88cdf6d7ad1",
+    "url": "/static/media/dribble.da2aac10.svg"
+  },
+  {
+    "revision": "16f6eeb4dd34ec1367fd942718d14bfc",
+    "url": "/static/media/react.16f6eeb4.svg"
+  },
+  {
+    "revision": "f18023dce4a1ce45b2f977f9b7598404",
+    "url": "/static/media/redux.f18023dc.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/javascript.0cded3a3.png"
+  },
+  {
+    "revision": "1f7054ae41d211bab5870ade7d8688ce",
+    "url": "/static/media/sass.1f7054ae.png"
+  },
+  {
+    "revision": "409370a1e41dfe095d7a69e7dc369bbc",
+    "url": "/static/media/mongo.409370a1.png"
+  },
+  {
+    "revision": "71621447c1806c207770ed3135e4ef05",
+    "url": "/static/media/footer.71621447.svg"
+  },
+  {
+    "revision": "93f7a700e7d62693ba92c5705babb670",
+    "url": "/static/media/noblyCard.93f7a700.png"
+  },
+  {
+    "revision": "519953b531fcbb9b379be7e2dbdb7d05",
+    "url": "/static/media/noblyWorkBg.519953b5.jpg"
+  },
+  {
+    "revision": "f60a44a4c68dff90958e1335d2571ff6",
+    "url": "/static/media/noblyWork01.f60a44a4.png"
+  },
+  {
+    "revision": "43fd5e696f474c69d4b8a52a9d52687d",
+    "url": "/static/media/noblyWork02.43fd5e69.png"
+  },
+  {
+    "revision": "5bb690db650033b5f16c3f81e509e3d8",
+    "url": "/static/media/noblyWork03.5bb690db.png"
+  },
+  {
+    "revision": "95123bfb29bee37fb06b65193b100c0b",
+    "url": "/static/media/integraCard.95123bfb.jpg"
+  },
+  {
+    "revision": "577dc0b2443a1a84eec743dadec82ec7",
+    "url": "/static/media/integraWorkBg.577dc0b2.jpg"
+  },
+  {
+    "revision": "6f8dfa49d0e1de5db0aa7584ae5a57ea",
+    "url": "/static/media/integraWork01.6f8dfa49.jpg"
+  },
+  {
+    "revision": "5a771c764e62170a23a0760e43011c5f",
+    "url": "/static/media/integraWork02.5a771c76.jpg"
+  },
+  {
+    "revision": "e0cc8b21298efda07ba6",
+    "url": "/static/js/main.e0cc8b21.chunk.js"
+  },
+  {
+    "revision": "ae7fc194412b8a7abdfa75c89ba7aa93",
+    "url": "/static/media/noodleCardBg.ae7fc194.jpg"
+  },
+  {
+    "revision": "0e60b10464612ea525d4b625e0235f75",
+    "url": "/static/media/noodleWorkBg.0e60b104.jpg"
+  },
+  {
+    "revision": "d9df897a5d59410665770972cbfe1f1b",
+    "url": "/static/media/noodleWork01.d9df897a.png"
+  },
+  {
+    "revision": "90effba24d6bdfdefc1f73c7964a2659",
+    "url": "/static/media/noodleWork02.90effba2.png"
+  },
+  {
+    "revision": "19168c088a2395afb16744a9df7c187b",
+    "url": "/static/media/noodleWork03.19168c08.png"
+  },
+  {
+    "revision": "d5e21129c803990715449f0925b8eea7",
+    "url": "/static/media/sinergyCardBg.d5e21129.jpg"
+  },
+  {
+    "revision": "b32745b5da561f9e82a036d4f54a3124",
+    "url": "/static/media/sinergyWorkBg.b32745b5.jpg"
+  },
+  {
+    "revision": "58360697633a1e794a362683151ead38",
+    "url": "/static/media/sinergyWork01.58360697.png"
+  },
+  {
+    "revision": "2a0e9cf19068326d12a3f847f06552b5",
+    "url": "/static/media/sinergyWork02.2a0e9cf1.png"
+  },
+  {
+    "revision": "0d325d441057a07735c05e361aa4e69f",
+    "url": "/static/media/sinergyWork03.0d325d44.png"
+  },
+  {
+    "revision": "e4d6e502fbd758f999c02042c529bb73",
+    "url": "/static/media/sinergyWork04.e4d6e502.png"
+  },
+  {
+    "revision": "dc7bae60cf79879dd3771b99bc7294f1",
+    "url": "/static/media/novaCardBg.dc7bae60.png"
+  },
+  {
+    "revision": "09c5fca91ffba4e7556b26a22cd12a0e",
+    "url": "/static/media/novaWorkBg.09c5fca9.png"
+  },
+  {
+    "revision": "14c3b86ae09c55693e4f77fe05017b3f",
+    "url": "/static/media/novaWork01.14c3b86a.png"
+  },
+  {
+    "revision": "e121059ab2bbaf6e3e51fd394de1207c",
+    "url": "/static/media/novaWork02.e121059a.png"
+  },
+  {
+    "revision": "b8f81a39e693b0405e3df24875d323b7",
+    "url": "/static/media/novaWork03.b8f81a39.png"
+  },
+  {
+    "revision": "d8ca90b78f8ba41068e1812a43647573",
+    "url": "/static/media/novaWork04.d8ca90b7.png"
+  },
+  {
+    "revision": "7f0494c8314282c0aa76b988673f4abe",
+    "url": "/static/media/novaWork05.7f0494c8.png"
+  },
+  {
+    "revision": "a494244ba1586e640cd853bb959793e9",
+    "url": "/static/media/stCardBg.a494244b.jpg"
+  },
+  {
+    "revision": "ecb13cf9cd773fd9647f549408e1cddb",
+    "url": "/static/media/stWorkBg.ecb13cf9.png"
+  },
+  {
+    "revision": "fda5bf91703cb3f3a1760ad061d85be5",
+    "url": "/static/media/stWork01.fda5bf91.png"
+  },
+  {
+    "revision": "deb5f9165a40f123678a1ae655d0e76e",
+    "url": "/static/media/stWork02.deb5f916.png"
+  },
+  {
+    "revision": "0bc218af9bf66033cb70962d38e39829",
+    "url": "/static/media/leftArrow.0bc218af.svg"
+  },
+  {
+    "revision": "6a9d8a522795945c68c00b5ccb421987",
+    "url": "/static/media/resume-english.6a9d8a52.pdf"
+  },
+  {
+    "revision": "f895e8638216744bd5fe9b48d1672313",
+    "url": "/static/media/jm.f895e863.png"
+  },
+  {
+    "revision": "bf5966e7ebdb82aad5cbf3e9aebe2587",
+    "url": "/index.html"
+  }
+];
